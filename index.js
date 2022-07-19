1e6fa9f24721a6c44c219bee5b5a6435f78e9434
guillescharf <scharf.guillermo@gmail.com>
2022-07-19 00:33:48 -0300

    :tada: JS Excercice

---
+++ index.js
@@ -0,0 +1,12 @@
+const miNombre = "Guillermo Scharf";
+const miEdad = 37;
+const enPareja = true;
+const arrayNumeros = [1,2,3,4,5,6,7,8,9,10];
+const arraySkillers = ["Emmanuel Mombelli", "Diego Patruno", "Marina Arrieta", "Georgina Bosque", "Mariano Perez"];
+const objetoDatos = {
+    miNombre: "Guillermo Scharf",
+    miEdad: 37,
+    enPareja: true,
+    arrayNumeros: [1,2,3,4,5,6,7,8,9,10],
+    arraySkillers: ["Emmanuel Mombelli", "Diego Patruno", "Marina Arrieta", "Georgina Bosque", "Mariano Perez"]
+}
