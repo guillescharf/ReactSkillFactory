b2e3a7c479b410878bb56b2bfe1beecfe150bf0a
guillescharf <scharf.guillermo@gmail.com>
2022-07-20 10:53:20 -0300

    :rocket: Added console.log lines

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ const objetoDatos = {
     enPareja: true,
     arrayNumeros: [1,2,3,4,5,6,7,8,9,10],
     arraySkillers: ["Emmanuel Mombelli", "Diego Patruno", "Marina Arrieta", "Georgina Bosque", "Mariano Perez"]
-}
+};
+console.log(miNombre);
+console.log(miEdad);
+console.log(enPareja);
+console.log(arrayNumeros);
+console.log(arraySkillers);
+console.log(objetoDatos);
